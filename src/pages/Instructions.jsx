21c4752885b498b4f7fec14e533ff81d8d0f28e8
Brandon Upchurch <brandonup@gmail.com>
2025-05-16 11:14:10 -0700

    Separate instruction notes into new TextBox

--- src/pages/Instructions.jsx
+++ src/pages/Instructions.jsx
@@ -6,7 +6,13 @@ const Instructions = () => {
       <TextBox
         id="instructions-textbox"
         label="Instructions"
-        content="1) Create a Replit account.\n2) Decide on an MVP app idea.\n3) Write a short, 2-4 sentence summary of your MVP.\n4) On the “Prompt Examples” page, use the “Product Manager” example to create your first prompt. (Fill in the blanks)\n5) Enter this prompt into your chatbot to create your requirements. \n6) Go back to the “Prompt Examples” page and use the “Software Developer” example to create your second prompt. (fill in the blanks and paste in the requirements from the previous step).\n7) Enter this second prompt into your chatbot to create your final prompt.\n8) Open Repit, paste your prompt in the the chat window, and click “Start Building”.\n9) Approve the plan and wait for it to build your MVP. (1-5 minutes) \n10) Once complete, test your app and troubleshoot any bugs.\n11) If you have time, ask Replit to add one more very simple feature.\n\n- If asked to create a database, say “no”.\n- If asked for an API Key, use the key found on the “API Key” page."
+        content="1) Create a Replit account.\n2) Decide on an MVP app idea.\n3) Write a short, 2-4 sentence summary of your MVP.\n4) On the “Prompt Examples” page, use the “Product Manager” example to create your first prompt. (Fill in the blanks)\n5) Enter this prompt into your chatbot to create your requirements. \n6) Go back to the “Prompt Examples” page and use the “Software Developer” example to create your second prompt. (fill in the blanks and paste in the requirements from the previous step).\n7) Enter this second prompt into your chatbot to create your final prompt.\n8) Open Repit, paste your prompt in the the chat window, and click “Start Building”.\n9) Approve the plan and wait for it to build your MVP. (1-5 minutes) \n10) Once complete, test your app and troubleshoot any bugs.\n11) If you have time, ask Replit to add one more very simple feature."
+        asList={true}
+      />
+      <TextBox
+        id="instructions-notes-textbox"
+        label="Important Notes"
+        content="- If asked to create a database, say “no”.\n- If asked for an API Key, use the key found on the “API Key” page."
         asList={true}
       />
     </Section>
